3c56435e87538836194ac361522b7983bbfc3323
Tran Thi Tram <tram.auto.1010@gmail.com>
2026-04-03 16:15:28 +0900

    add bai tap

--- tests/lesson-02/ex4.js
+++ tests/lesson-02/ex4.js
@@ -1,8 +1,13 @@
 const height = 153;
 if (height > 100)
 {
+    //số lẻ của chiều cao - lấy 2 số sau (cân nặng tối đa)
     const maxWeight = height%100;
+
+    //công thức cân nặng lý tưởng
     const idealWeight = (maxWeight*9)/10;
+
+    //công thức cân nặng tối thiểu
     const minWeight = (maxWeight*8)/10;
     console.log(`Cân nặng lý tưởng: ${idealWeight}kg, Cân nặng tối đa: ${maxWeight}kg, Cân nặng tối thiểu: ${minWeight}kg`);
 }
